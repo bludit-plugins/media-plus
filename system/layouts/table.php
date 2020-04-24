3e36c340c6e11542b1c10ad6f58fc858d4b9be1c
SamBrishes <sam@pytes.net>
2020-04-24 14:30:36 +0200

    Update + Page Folder Button on Modal

--- system/layouts/table.php
+++ system/layouts/table.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  |  @copyright  Copyright © 2019 - 2020 pytesNET <info@pytes.net>
  */
 ?>
-<table class="table mt-4 media-list" data-action="<?php echo $this->buildURL("media/upload") ?>" data-path="<?php echo MediaManager::slug($path); ?>" data-token="<?php echo $security->getTokenCSRF(); ?>">
+<table class="table mt-4 media-list" data-action="<?php echo $this->buildURL("media/upload") ?>" data-path="<?php echo $path; ?>" data-token="<?php echo $security->getTokenCSRF(); ?>">
     <thead>
         <tr>
             <th width="30px" class="th-checkbox" scope="col"></th>
